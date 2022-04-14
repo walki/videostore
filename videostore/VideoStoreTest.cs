12fc6dcdd52f459c0c0a498a40dd1de05b98b38a
Roger Walker <roger.john.walker@gmail.com>
2022-04-14 19:57:21 -0400

    Quick test formatting and rename

--- videostore/VideoStoreTest.cs
+++ videostore/VideoStoreTest.cs
@@ -19,7 +19,7 @@ namespace videostore
         }
 
         [Test]
-        public void testSingleNewReleaseStatement()
+        public void SingleNewReleaseStatement()
         {
             customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
             Assert.That(customer.statement(), Is.EqualTo("Rental Record for Fred\n" +
@@ -29,28 +29,39 @@ namespace videostore
         }
 
         [Test]
-        public void testDualNewReleaseStatement()
+        public void DualNewReleaseStatement()
         {
             customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
             customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
-            Assert.That(customer.statement(), Is.EqualTo("Rental Record for Fred\n\tThe Cell\t9.00\n\tThe Tigger Movie\t9.00\nYou owed 18.00\nYou earned 4 frequent renter points\n"));
+            Assert.That(customer.statement(), Is.EqualTo("Rental Record for Fred\n" +
+                                                        "\tThe Cell\t9.00\n" +
+                                                        "\tThe Tigger Movie\t9.00\n" +
+                                                        "You owed 18.00\n" +
+                                                        "You earned 4 frequent renter points\n"));
         }
 
         [Test]
-        public void testSingleChildrensStatement()
+        public void SingleChildrensStatement()
         {
             customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
-            Assert.That(customer.statement(), Is.EqualTo("Rental Record for Fred\n\tThe Tigger Movie\t1.50\nYou owed 1.50\nYou earned 1 frequent renter points\n"));
+            Assert.That(customer.statement(), Is.EqualTo("Rental Record for Fred\n" +
+                                                        "\tThe Tigger Movie\t1.50\n" +
+                                                        "You owed 1.50\nYou earned 1 frequent renter points\n"));
         }
 
         [Test]
-        public void testMultipleRegularStatement()
+        public void MultipleRegularStatement()
         {
             customer.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
             customer.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
             customer.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
 
-            Assert.That(customer.statement(), Is.EqualTo("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.00\n\t8 1/2\t2.00\n\tEraserhead\t3.50\nYou owed 7.50\nYou earned 3 frequent renter points\n"));
+            Assert.That(customer.statement(), Is.EqualTo("Rental Record for Fred\n" +
+                                                        "\tPlan 9 from Outer Space\t2.00\n" +
+                                                        "\t8 1/2\t2.00\n" +
+                                                        "\tEraserhead\t3.50\n" +
+                                                        "You owed 7.50\n" +
+                                                        "You earned 3 frequent renter points\n"));
         }
 
     }
